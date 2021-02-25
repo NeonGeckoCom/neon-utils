694600abbf3f6d06fa6e1b9ed741508a41a085b4
d_mcknight <daniel@neongecko.com>
2021-02-25 15:03:25 -0800

    Fix location_util_tests.py imports for GH Actions compat.

--- tests/location_util_tests.py
+++ tests/location_util_tests.py
@@ -17,10 +17,11 @@
 # US Patents 2008-2021: US7424516, US20140161250, US20140177813, US8638908, US8068604, US8553852, US10530923, US10530924
 # China Patent: CN102017585  -  Europe Patent: EU2156652  -  Patents Pending
 
-import unittest
+import sys
 import os
+import unittest
 
-ROOT_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from neon_utils.location_utils import *
 
 
