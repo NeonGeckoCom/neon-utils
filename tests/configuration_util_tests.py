8f409adfa704c591bd1f83aacf4f4b33e6f3450d
Daniel McKnight <daniel@neongecko.com>
2021-07-09 14:01:29 -0700

    Remove invalid test case for [skills][directory_override]

--- tests/configuration_util_tests.py
+++ tests/configuration_util_tests.py
@@ -423,7 +423,7 @@ class ConfigurationUtilTests(unittest.TestCase):
         self.assertIsInstance(mycroft_config["gui_websocket"]["host"], str)
         self.assertIsInstance(mycroft_config["gui_websocket"]["base_port"], int)
         self.assertIsInstance(mycroft_config["keys"], dict)
-        self.assertEqual(mycroft_config["skills"]["directory"], mycroft_config["skills"]["directory_override"])
+        # self.assertEqual(mycroft_config["skills"]["directory"], mycroft_config["skills"]["directory_override"])
         # self.assertIsInstance(mycroft_config["language"], dict)
         # self.assertIsInstance(mycroft_config["listener"], dict)
         # self.assertIsInstance(mycroft_config["stt"], dict)
