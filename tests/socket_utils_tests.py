7226f70c19412ea1e3c5aa70430636647fb1693f
kirill <kirill.grim@gmail.com>
2021-07-16 12:15:22 +0300

    Added get packet data testing

--- tests/socket_utils_tests.py
+++ tests/socket_utils_tests.py
@@ -19,8 +19,11 @@
 import os
 import sys
 import unittest
+import socket
+import threading
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+from neon_utils import LOG
 from neon_utils.socket_utils import *
 
 TEST_DICT = {b"section 1": {"key1": "val1",
@@ -30,8 +33,19 @@ TEST_DICT = {b"section 1": {"key1": "val1",
 
 TEST_DICT_B64 = b'IntiJ3NlY3Rpb24gMSc6IHsna2V5MSc6ICd2YWwxJywgJ2tleTInOiAndmFsMid9LCAnc2VjdGlvbiAyJzogeydrZXlfMSc6IGIndmFsMScsICdrZXlfMic6ICd2YWwyJ319Ig=='
 
+TEST_SOCKET_ADDRESS = ('127.0.0.1', 8999)
+
 
 class SocketUtilsTest(unittest.TestCase):
+
+    def tcp_client(self):
+        """Simple tcp socket client emitting just one message"""
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect(TEST_SOCKET_ADDRESS)
+            s.sendall(TEST_DICT_B64)
+            LOG.info('Client sent data')
+            data = get_packet_data(socket=s, sequentially=True)
+            self.assertEqual(data, TEST_DICT_B64)
 
     def test_01_dict_to_b64(self):
         b64_str = dict_to_b64(TEST_DICT)
@@ -44,3 +58,15 @@ class SocketUtilsTest(unittest.TestCase):
         self.assertIsInstance(result_dict, dict)
         self.assertTrue(len(list(result_dict)) > 0)
         self.assertEqual(result_dict, TEST_DICT)
+
+    def test_03_get_packet_data(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind(TEST_SOCKET_ADDRESS)
+            s.listen()
+            threading.Thread(target=self.tcp_client).start()
+            conn, addr = s.accept()
+            with conn:
+                LOG.info(f'Connected by {addr}')
+                data = get_packet_data(conn, sequentially=False)
+                self.assertEqual(data, TEST_DICT_B64)
+                conn.sendall(data)
