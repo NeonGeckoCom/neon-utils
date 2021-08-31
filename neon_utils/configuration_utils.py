97da3023f4597d12ee66cb5628cc9e4e16b1f10e
Daniel McKnight <daniel@neongecko.com>
2021-08-31 13:22:24 -0700

    Prevent overwriting hotwords config and add unit test

--- neon_utils/configuration_utils.py
+++ neon_utils/configuration_utils.py
@@ -454,7 +454,7 @@ def dict_make_equal_keys(dct_to_change: MutableMapping, keys_dct: MutableMapping
         raise ValueError("Empty keys_dct provided, not modifying anything.")
     for key in list(dct_to_change.keys()):
         if isinstance(keys_dct.get(key), dict) and isinstance(dct_to_change[key], MutableMapping):
-            if max_depth > cur_depth and key not in ("tts", "stt"):
+            if max_depth > cur_depth and key not in ("tts", "stt", "hotwords"):
                 dct_to_change[key] = dict_make_equal_keys(dct_to_change[key], keys_dct[key], max_depth, cur_depth + 1)
         elif key not in keys_dct.keys():
             dct_to_change.pop(key)
