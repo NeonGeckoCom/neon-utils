30546f2a506e701976c70a9630bf45af320a4cac
Daniel McKnight <daniel@neon.ai>
2022-02-23 15:18:40 -0800

    replace settings init

--- neon_utils/skills/mycroft_skill.py
+++ neon_utils/skills/mycroft_skill.py
@@ -100,15 +100,15 @@ class PatchedMycroftSkill(MycroftSkill):
                 os.rmdir(self.file_system.path)
             self.file_system.path = fs_path
 
-    # def _init_settings(self):
-    #     self.settings_write_path = self.file_system.path
-    #     skill_settings = get_local_settings(self.settings_write_path, self.name)
-    #     settings_from_disk = dict(skill_settings)
-    #     self._settings = dict_update_keys(skill_settings, self._read_default_settings())
-    #     if self._settings != settings_from_disk:
-    #         with open(os.path.join(self.settings_write_path, 'settings.json'), "w+") as f:
-    #             json.dump(self._settings, f, indent=4)
-    #     self._initial_settings = dict(self.settings)
+    def _init_settings(self):
+        self.settings_write_path = self.file_system.path
+        skill_settings = get_local_settings(self.settings_write_path, self.name)
+        settings_from_disk = dict(skill_settings)
+        self._settings = dict_update_keys(skill_settings, self._read_default_settings())
+        if self._settings != settings_from_disk:
+            with open(os.path.join(self.settings_write_path, 'settings.json'), "w+") as f:
+                json.dump(self._settings, f, indent=4)
+        self._initial_settings = dict(self.settings)
 
     def _read_default_settings(self):
         yaml_path = os.path.join(self.root_dir, "settingsmeta.yml")
