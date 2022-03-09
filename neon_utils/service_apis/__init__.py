c46a0d2c8c3d9eb5cd1dc48f198df6af8c64375e
Daniel McKnight <daniel@neon.ai>
2022-03-08 18:26:20 -0800

    Deprecate service api modules and import from neon_api_proxy directly

--- neon_utils/service_apis/__init__.py
+++ neon_utils/service_apis/__init__.py
@@ -26,43 +26,6 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from enum import Enum
-from neon_utils.configuration_utils import get_neon_auth_config
-from neon_utils.mq_utils import send_mq_request
-
-AUTH_CONFIG = get_neon_auth_config()
-
-
-class NeonAPI(Enum):
-    def __str__(self):
-        return self.value
-
-    ALPHA_VANTAGE = "alpha_vantage"
-    OPEN_WEATHER_MAP = "open_weather_map"
-    WOLFRAM_ALPHA = "wolfram_alpha"
-    FINANCIAL_MODELING_PREP = "financial_modeling_prep"
-    NOT_IMPLEMENTED = "not_implemented"
-    TEST_API = "api_test_endpoint"
-
-
-def request_neon_api(api: NeonAPI, query_params: dict, timeout: int = 30) -> dict:
-    """
-        Handle a request for information from the Neon API Proxy Server
-        :param api: Service API to target
-        :param query_params: Data parameters to pass to service API
-        :param timeout: Request timeout in seconds
-        :return: dict response from API with: `status_code`, `content`, and `encoding`
-    """
-
-    if not isinstance(api, NeonAPI):
-        raise TypeError(f"Expected a NeonAPI, got: {api}")
-    if not query_params:
-        raise ValueError("Got empty query params")
-    if not isinstance(query_params, dict):
-        raise TypeError(f"Expected dict, got: {query_params}")
-
-    request_data = {**query_params, **{"service": str(api)}}
-    response = send_mq_request("/neon_api", request_data, "neon_api_input", "neon_api_output", timeout)
-    return response or {"status_code": 401,
-                        "content": f"Neon API failed to give a response within {timeout} seconds",
-                        "encoding": None}
+from neon_utils.logger import LOG
+from neon_api_proxy.client import NeonAPI, request_api as request_neon_api
+LOG.warning("This reference is deprecated, use neon_api_proxy.client directly")
--- neon_utils/service_apis/alpha_vantage.py
+++ neon_utils/service_apis/alpha_vantage.py
@@ -26,105 +26,13 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import json
-import urllib.parse
-from json import JSONDecodeError
-
 import requests_cache as requests
 
-from neon_utils.logger import LOG
 from requests.adapters import HTTPAdapter
-from . import AUTH_CONFIG, NeonAPI, request_neon_api
+from neon_utils.logger import LOG
+from neon_api_proxy.client.alpha_vantage import search_stock_by_name, get_stock_quote
 
-SESSION = requests.CachedSession(backend='memory', cache_name="alpha_vantage")
-SESSION.mount('http://', HTTPAdapter(max_retries=8))
-SESSION.mount('https://', HTTPAdapter(max_retries=8))
-
-
-def search_stock_by_name(company: str, **kwargs) -> list:
-    """
-    Queries Alpha Vantage for stocks matching the specified company
-    :param company: Company name/stock search term
-    :param kwargs:
-      'api_key' - optional str api_key to use for query (None to force remote lookup)
-      'region' - optional preferred region (default `United States`)
-    :return: list of dict matched stock data
-    """
-    api_key = kwargs.get("api_key", AUTH_CONFIG.get("alpha_vantage", {}).get("api_key"))
-    region = kwargs.get("region") or "United States"
-
-    if api_key:
-        query_params = {"function": "SYMBOL_SEARCH",
-                        "keywords": company,
-                        "apikey": api_key}
-        resp = query_alpha_vantage_api(f"https://www.alphavantage.co/query?{urllib.parse.urlencode(query_params)}")
-    else:
-        query_params = {**kwargs, **{"api": "symbol", "company": company}}
-        resp = request_neon_api(NeonAPI.ALPHA_VANTAGE, query_params)
-
-    if resp["status_code"] == -1:
-        data = {"error": resp["content"]}
-    else:
-        try:
-            data = json.loads(resp["content"])
-        except JSONDecodeError:
-            data = {"error": "Error decoding response",
-                    "response": resp}
-    if data.get("Information"):
-        LOG.warning(data.get("Information"))
-        # TODO: Handle API Errors DM
-    if not data.get("bestMatches"):
-        LOG.warning(f"No matches found for {company}")
-        return []
-    filtered_data = [stock for stock in data.get("bestMatches") if stock.get("4. region") == region]
-    if not filtered_data:
-        filtered_data = data.get("bestMatches")
-    data = [{"symbol": stock.get("1. symbol"),
-             "name": stock.get("2. name"),
-             "region": stock.get("4. region"),
-             "currency": stock.get("8. currency")} for stock in filtered_data]
-    return data
-
-
-def get_stock_quote(symbol: str, **kwargs) -> dict:
-    """
-    Queries Alpha Vantage for stock information for the specified company
-    :param symbol: Stock ticker symbol
-    :param kwargs:
-      'api_key' - optional str api_key to use for query (None to force remote lookup)
-    :return: dict stock data
-    """
-    api_key = kwargs.get("api_key", AUTH_CONFIG.get("alpha_vantage", {}).get("api_key"))
-
-    if api_key:
-        query_params = {"function": "GLOBAL_QUOTE",
-                        "symbol": symbol,
-                        "apikey": api_key}
-        resp = query_alpha_vantage_api(f"https://www.alphavantage.co/query?{urllib.parse.urlencode(query_params)}")
-    else:
-        query_params = {**kwargs, **{"api": "quote", "symbol": symbol}}
-        resp = request_neon_api(NeonAPI.ALPHA_VANTAGE, query_params)
-
-    if resp["status_code"] == -1:
-        data = {"error": resp["content"]}
-    else:
-        try:
-            data = json.loads(resp["content"])
-        except JSONDecodeError:
-            data = {"error": "Error decoding response",
-                    "response": resp}
-    if data.get("Information"):
-        LOG.warning(data.get("Information"))
-        # TODO: Handle API Errors DM
-
-    if not data.get("Global Quote"):
-        LOG.warning(f"No data found for {symbol}")
-        data["error"] = data.get("error") or "No data found"
-        LOG.error(data)
-        return data
-    return {"symbol": data.get("Global Quote")["01. symbol"],
-            "price": data.get("Global Quote")["05. price"],
-            "close": data.get("Global Quote")["08. previous close"]}
+LOG.warning("This reference is deprecated, use neon_api_proxy.client directly")
 
 
 def query_alpha_vantage_api(url: str) -> dict:
@@ -133,6 +41,10 @@ def query_alpha_vantage_api(url: str) -> dict:
     :param url: Alpha Vantage API URL to query
     :return: dict status_code, content, encoding
     """
+    SESSION = requests.CachedSession(backend='memory',
+                                     cache_name="alpha_vantage")
+    SESSION.mount('http://', HTTPAdapter(max_retries=8))
+    SESSION.mount('https://', HTTPAdapter(max_retries=8))
     if "global_quote" in url.lower():
         expiration = 5*60  # Cache quotes for 5 minutes
     elif "symbol_search" in url.lower():
