10fb8260ebee6ef193f04e02aaf3582cd5973329
Daniel McKnight <daniel@neon.ai>
2022-06-27 15:05:10 -0700

    Fix error in container config handling

--- neon_utils/configuration_utils.py
+++ neon_utils/configuration_utils.py
@@ -563,7 +563,7 @@ def _validate_config_env():
             # TODO: Write unit test for this DM
             LOG.warning("NEON_CONFIG_PATH is not writable, copying config")
             shutil.copy(f'{os.environ["NEON_CONFIG_PATH"]}/*',
-                        os.environ["XDG_CONFIG_HOME"])
+                        join(get_config_dir(), "neon"))
     else:
         LOG.info("Setting NEON_CONFIG_PATH to xdg default ~/.config/neon")
         os.environ["NEON_CONFIG_PATH"] = expanduser("~/.config/neon")
