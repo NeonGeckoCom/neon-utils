009626a5d8937dd2e35ed6edb1ac8fd83fbeff75
Daniel McKnight <daniel@neon.ai>
2022-06-28 15:40:57 -0700

    Loosen expected exception in MQ tests

--- tests/mq_util_tests.py
+++ tests/mq_util_tests.py
@@ -134,7 +134,7 @@ class MqUtilTests(unittest.TestCase):
             self.assertTrue(resp['success'], resp.get('reason'))
 
     def test_send_mq_request_invalid_vhost(self):
-        with self.assertRaises(ValueError):
+        with self.assertRaises(Exception):
             send_mq_request("invalid_endpoint", {}, "test", "test")
 
     # def test_get_mq_response_error_in_handler(self):
