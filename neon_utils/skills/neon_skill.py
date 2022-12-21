89df8ea3b1467e9ec62a7ec4ad4cd01bd8a2e74b
Daniel McKnight <daniel@neongecko.com>
2022-12-21 11:57:28 -0800

    Fix skill class import error with optional MQ dependency

--- neon_utils/skills/neon_skill.py
+++ neon_utils/skills/neon_skill.py
@@ -45,16 +45,22 @@ from neon_utils.location_utils import to_system_time
 from neon_utils.logger import LOG
 from neon_utils.message_utils import dig_for_message, resolve_message
 from neon_utils.cache_utils import LRUCache
-from neon_utils.mq_utils import send_mq_request
 from neon_utils.skills.mycroft_skill import PatchedMycroftSkill as MycroftSkill
 from neon_utils.file_utils import resolve_neon_resource_file
 from neon_utils.user_utils import get_user_prefs
 from mycroft.skills.settings import save_settings
 
 try:
+    from neon_utils.mq_utils import send_mq_request
+except ImportError:
+    LOG.warning("MQ Dependencies not installed")
+    send_mq_request = None
+
+try:
     from ovos_plugin_manager.language import OVOSLangDetectionFactory, \
         OVOSLangTranslationFactory
-except ImportError as e:
+except ImportError:
+    LOG.warning("Language Dependencies not installed")
     OVOSLangDetectionFactory, OVOSLangTranslationFactory = None, None
 
 # TODO if accepted, make changes to QASkill and WikipediaSkill
@@ -332,7 +338,7 @@ class NeonSkill(MycroftSkill):
         if not email_addr and message:
             email_addr = get_user_prefs(message)["user"].get("email")
 
-        if email_addr:
+        if email_addr and send_mq_request:
             LOG.info("Send email via Neon Server")
             request_data = {"recipient": email_addr,
                             "subject": title,
