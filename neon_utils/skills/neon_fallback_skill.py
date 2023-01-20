61db091c5558fccb2e794b190911811d0ff53fc6
Daniel McKnight <daniel@neongecko.com>
2023-01-19 16:41:39 -0800

    Fix `IntentLayers` import

--- neon_utils/skills/neon_fallback_skill.py
+++ neon_utils/skills/neon_fallback_skill.py
@@ -30,6 +30,7 @@ from neon_utils.skills.neon_skill import NeonSkill
 from ovos_workshop.skills.fallback import FallbackSkill
 from ovos_workshop.skills.ovos import OVOSSkill
 from ovos_utils.intents import IntentLayers
+from ovos_workshop.skills.layers import IntentLayers
 
 
 class NeonFallbackSkill(FallbackSkill, NeonSkill, OVOSSkill):
