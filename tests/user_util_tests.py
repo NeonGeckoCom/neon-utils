NeonGeckoCom/neon-utils

commit 46d38696d3e8c0a112c85682d56e6c8990d97d61

ovos-config compat. fixes (#424)
* Test ovos-utils changes

* Test ovos-config changes

* Update requirements to patch bug in package util tests

* Troubleshooting config handling in unit tests

* Troubleshooting config handling in unit tests

* Troubleshooting config handling in user util tests

* Troubleshooting config handling in user util tests

* Remove importlib patching and explicitly patch configuration

* Fix config patching

* Add more debug logging and exception handling

* More test troubleshooting

* Replace importlib patching

* More debugging and patching

* Troubleshooting config patching in tests

* Troubleshooting skill object test failures

* More skill test troubleshooting

* More skill test troubleshooting

* Handle empty Mycroft location config

* Cleanup test changes

* Cleanup changes

* Update ovos-config dependency to PyPI spec

neon_utils/configuration_utils.py
@@ -810,6 +810,7 @@ def get_user_config_from_mycroft_conf(user_config: dict = None) -> dict:
                            os.path.join(os.path.dirname(__file__),
                                         "default_configurations")).content)
     mycroft_config = MycroftUserConfig()
+    LOG.debug(f"Initializing mycroft config at {mycroft_config.path}")
     user_config["speech"]["stt_language"] = mycroft_config.get("lang", "en-us")
     user_config["speech"]["tts_language"] = mycroft_config.get("lang", "en-us")
     user_config["speech"]["alt_languages"] = \
@@ -832,6 +833,9 @@ def get_user_config_from_mycroft_conf(user_config: dict = None) -> dict:
             "tz": mycroft_config["location"]["timezone"]["code"],
             "utc": str(round(mycroft_config["location"]["timezone"]["offset"]
                              / 3600000, 1))}
+    else:
+        LOG.warning(f"No location in config: {mycroft_config.path}")
+        user_config["location"] = {}
     return user_config
 
 
@@ -891,8 +895,8 @@ def get_mycroft_compatible_location(location: dict) -> dict:
     :returns: dict formatted to match mycroft.conf spec
     """
     from neon_utils.parse_utils import clean_quotes
-    if not any((location['lat'], location['lng'],
-                location['city'], location['tz'])):
+    if not any((location.get('lat'), location.get('lng'),
+                location.get('city'), location.get('tz'))):
         LOG.debug('Neon config empty, return core value')
         return _safe_mycroft_config().get('location')
     try:

tests/user_util_tests.py
@@ -71,13 +71,18 @@ class UserUtilTests(unittest.TestCase):
         wrapper(test_message_2, user_2)
 
     def test_get_default_user_config_from_mycroft_conf(self):
+        # Patch configuration for test
         test_config_dir = os.path.join(os.path.dirname(__file__),
                                        "user_util_test_config")
-        os.environ["NEON_CONFIG_PATH"] = test_config_dir
         os.environ["XDG_CONFIG_HOME"] = test_config_dir
         import importlib
-        from neon_utils import user_utils
-        importlib.reload(user_utils)
+        import ovos_config
+        importlib.reload(ovos_config.locations)
+        import ovos_config.models
+        importlib.reload(ovos_config.models)
+        import neon_utils.user_utils
+        importlib.reload(neon_utils.user_utils)
+
         from neon_utils.user_utils import get_default_user_config
         user_config = get_default_user_config()
         self.assertFalse(os.path.isfile(os.path.join(test_config_dir,
@@ -92,7 +97,6 @@ class UserUtilTests(unittest.TestCase):
                           "state": 'Washington',
                           "country": "United States"})
 
-        os.environ.pop("NEON_CONFIG_PATH")
         os.environ.pop("XDG_CONFIG_HOME")
 
     def test_update_user_profile(self):

tests/configuration_util_tests.py
@@ -670,8 +670,8 @@ class ConfigurationUtilTests(unittest.TestCase):
 
         user_config = get_neon_user_config(CONFIG_PATH)
 
-        with self.assertRaises(KeyError):
-            get_mycroft_compatible_location(user_config.content)
+        # with self.assertRaises(KeyError):
+        #     get_mycroft_compatible_location(user_config.content)
 
         # Default mycroft.conf
         location = get_mycroft_compatible_location(user_config["location"])
@@ -780,6 +780,10 @@ class ConfigurationUtilTests(unittest.TestCase):
         location['lat'] = '47.4799078'
         location['lng'] = '-122.2034496'
         self.assertIsInstance(get_mycroft_compatible_location(location), dict)
+
+        # Empty Mycroft config location
+        self.assertIsInstance(get_mycroft_compatible_location({}), dict)
+
         shutil.move(old_user_info, ngi_user_info)
 
     def test_get_user_config_from_mycroft_conf(self):

tests/neon_skill_tests.py
@@ -1037,6 +1037,7 @@ class NeonSkillTests(unittest.TestCase):
 
         bus = FakeBus()
         os.environ["XDG_CONFIG_HOME"] = cls.config_dir
+
         cls.skill = TestSkill()
         # Mock the skill_loader process
         if hasattr(cls.skill, "_startup"):

commit 5cc099249c701d44d0f7354c57fbdea785a160ac

Fix config handling to always include default location with unit tests Fixes bug introduced in #424

neon_utils/configuration_utils.py
@@ -835,7 +835,6 @@ def get_user_config_from_mycroft_conf(user_config: dict = None) -> dict:
                              / 3600000, 1))}
     else:
         LOG.warning(f"No location in config: {mycroft_config.path}")
-        user_config["location"] = {}
     return user_config
 
 

tests/user_util_tests.py
@@ -25,10 +25,11 @@
 # LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
+import shutil
 import sys
 import os
 import unittest
+from os.path import join, dirname
 from copy import deepcopy
 from threading import Event
 
@@ -203,6 +204,21 @@ class UserUtilTests(unittest.TestCase):
         os.remove(os.path.join(config_object.path,
                                f".{config_object.name}.tmp"))
         os.remove(config_object.file_path)
+
+    def test_get_default_user_config(self):
+        os.environ['XDG_CONFIG_HOME'] = join(dirname(__file__), "test_config")
+        from neon_utils.user_utils import get_default_user_config
+        import neon_utils.user_utils
+        neon_utils.user_utils._DEFAULT_USER_CONFIG = None
+        user_config = get_default_user_config()
+        self.assertEqual(set(user_config.keys()),
+                         {'user', 'brands', 'location', 'units', 'speech',
+                          'response_mode', 'privacy'})
+        self.assertEqual(set(user_config['location'].keys()),
+                         {'lat', 'lng', 'city', 'state', 'country', 'tz',
+                          'utc'})
+        shutil.rmtree(os.environ['XDG_CONFIG_HOME'])
+        os.environ.pop("XDG_CONFIG_HOME")
 
 
 if __name__ == '__main__':