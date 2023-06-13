3c429a6ab78150a271cf7ad05e5d5855ac1e58dd
Daniel McKnight <daniel@neongecko.com>
2023-06-12 18:36:51 -0700

    Refactor skill init to use args/kwargs

--- neon_utils/skills/neon_skill.py
+++ neon_utils/skills/neon_skill.py
@@ -74,8 +74,8 @@ CACHE_TIME_OFFSET = 24*60*60  # seconds in 24 hours
 
 
 class NeonSkill(PatchedMycroftSkill):
-    def __init__(self, name=None, bus=None, use_settings=True, **kwargs):
-        PatchedMycroftSkill.__init__(self, name, bus, use_settings, **kwargs)
+    def __init__(self, *args, **kwargs):
+        PatchedMycroftSkill.__init__(self, *args, **kwargs)
         self.cache_loc = os.path.join(xdg_cache_home(), "neon")
         os.makedirs(self.cache_loc, exist_ok=True)
         self.lru_cache = LRUCache()
