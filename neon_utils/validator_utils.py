8947253a6ea7cb92fabf151c646a2c5a297b4777
Daniel McKnight <daniel@neongecko.com>
2023-07-10 10:34:54 -0700

    Remove unused import

--- neon_utils/validator_utils.py
+++ neon_utils/validator_utils.py
@@ -26,7 +26,6 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from neon_utils import LOG
 from lingua_franca import parse, set_default_lang
 
 from ovos_workshop.skills.mycroft_skill import MycroftSkill
