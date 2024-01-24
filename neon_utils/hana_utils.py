b221bb6c491a858505d5b94de0ab09c7e062a03f
Daniel McKnight <daniel@neon.ai>
2024-01-23 16:50:32 -0800

    Add `hana_utils` with unit tests Update unit tests to test entire module in one step

---
+++ neon_utils/hana_utils.py
@@ -0,0 +1,139 @@
+# NEON AI (TM) SOFTWARE, Software Development Kit & Application Framework
+# All trademark and other rights reserved by their respective owners
+# Copyright 2008-2022 Neongecko.com Inc.
+# Contributors: Daniel McKnight, Guy Daniels, Elon Gasper, Richard Leeds,
+# Regina Bloomstine, Casimiro Ferreira, Andrii Pernatii, Kirill Hrymailo
+# BSD-3 License
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from this
+#    software without specific prior written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+# OR PROFITS;  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import requests
+import json
+
+from os.path import join, isfile
+from time import time
+from typing import Optional
+from ovos_utils.log import LOG
+from ovos_utils.xdg_utils import xdg_cache_home
+
+_client_config = {}
+_client_config_path = join(xdg_cache_home(), "neon", "hana_token.json")
+_headers = {}
+
+
+class ServerException(Exception):
+    """Exception class representing a backend server communication error"""
+
+
+def _init_client(backend_address: str):
+    """
+    Initialize request headers for making backend requests. If a local cache is
+    available it will be used, otherwise an auth request will be made to the
+    specified backend server
+    @param backend_address: Hana server URL to connect to
+    """
+    global _client_config
+    global _headers
+
+    if not _client_config:
+        if isfile(_client_config_path):
+            with open(_client_config_path) as f:
+                _client_config = json.load(f)
+        else:
+            get_token(backend_address)
+
+    if not _headers:
+        _headers = {"Authorization": f"Bearer {_client_config['access_token']}"}
+
+
+def get_token(backend_address: str, username: str = "guest",
+              password: str = "password"):
+    """
+    Get new auth tokens from the specified server. This will cache the returned
+    token, overwriting any previous data at the cache path.
+    @param backend_address: Hana server URL to connect to
+    @param username: Username to authorize
+    @param password: Password for specified username
+    """
+    global _client_config
+    # TODO: username/password from configuration
+    resp = requests.post(f"{backend_address}/auth/login",
+                         json={"username": username,
+                               "password": password})
+    if not resp.ok:
+        raise ServerException(f"Error logging into {backend_address}. "
+                              f"{resp.status_code}: {resp.text}")
+    _client_config = resp.json()
+    with open(_client_config_path, "w+") as f:
+        json.dump(_client_config, f, indent=2)
+
+
+def refresh_token(backend_address: str):
+    """
+    Get new tokens from the specified server using an existing refresh token
+    (if it exists). This will update the cached tokens and associated metadata.
+    @param backend_address: Hana server URL to connect to
+    """
+    global _client_config
+    _init_client(backend_address)
+    update = requests.post(f"{backend_address}/auth/refresh", json={
+        "access_token": _client_config.get("access_token"),
+        "refresh_token": _client_config.get("refresh_token"),
+        "client_id": _client_config.get("client_id")})
+    if not update.ok:
+        raise ServerException(f"Error updating token from {backend_address}. "
+                              f"{update.status_code}: {update.text}")
+    _client_config = update.json()
+    with open(_client_config_path, "w+") as f:
+        json.dump(_client_config, f, indent=2)
+
+
+def request_backend(endpoint: str, request_data: dict,
+                    server_config: Optional[dict] = None) -> dict:
+    """
+    Make a request to a Hana backend server and return the json response
+    @param endpoint: server endpoint to query
+    @param request_data: dict data to send in request body
+    @param server_config: Configuration['server']. If None, this will be read
+        from configuration
+    @returns: dict response
+    """
+    if not server_config:
+        from ovos_config.config import Configuration
+        server_config = Configuration().get("server", {})
+    if not server_config.get("backend_type") == "hana":
+        backend_address = "https://hana.neonaialpha.com"
+    else:
+        backend_address = server_config.get("url")
+    _init_client(backend_address)
+    if time() >= _client_config.get("expiration", 0):
+        try:
+            refresh_token(backend_address)
+        except ServerException as e:
+            LOG.error(e)
+            get_token(backend_address)
+    resp = requests.post(f"{backend_address}/{endpoint.lstrip('/')}",
+                         json=request_data, headers=_headers)
+    if resp.ok:
+        return resp.json()
+    else:
+        raise ServerException(f"Error response {resp.status_code}: {resp.text}")
