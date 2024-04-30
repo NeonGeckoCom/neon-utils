939829c6f2c201d17d428d7a9b78c2fa1a47a602
Daniel McKnight <daniel@neon.ai>
2024-04-30 10:20:08 -0700

    Handle `post` releases with updated test

--- neon_utils/packaging_utils.py
+++ neon_utils/packaging_utils.py
@@ -292,6 +292,9 @@ def get_installed_prereleases() -> List[Tuple[str, str]]:
             continue
         name, version = line.split()
         if not version.replace('.', '').isnumeric():
+            if "post" in version:
+                LOG.debug(f"post release {name}:{version}")
+                continue
             prerelease_pkgs.append((name, version))
     return prerelease_pkgs
 
