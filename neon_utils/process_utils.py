6a09495328135121962e5ba1d5ed8aa295c25683
Daniel McKnight <daniel@neon.ai>
2025-06-17 18:48:19 -0700

    Implement HTTP status server helper method `process_utils.start_health_check_server`

--- neon_utils/process_utils.py
+++ neon_utils/process_utils.py
@@ -31,7 +31,7 @@ import tracemalloc
 from threading import Event, Thread
 from typing import Optional
 from ovos_utils.log import LOG
-
+from ovos_utils.process_utils import ProcessStatus, ProcessState
 
 _malloc_event = Event()
 _malloc_thread = None
@@ -44,43 +44,120 @@ def start_systemd_service(service: callable, **kwargs):
     try:
         import sdnotify
     except ImportError:
-        LOG.exception(f'sdnotify not installed! '
-                      f'Starting service without systemd hooks')
+        LOG.exception(
+            "sdnotify not installed! Starting service without systemd hooks"
+        )
         service(**kwargs)
         return
     notifier = sdnotify.SystemdNotifier()
 
     def on_ready():
-        notifier.notify('READY=1')
-        notifier.notify('STATUS=Ready')
+        notifier.notify("READY=1")
+        notifier.notify("STATUS=Ready")
 
     def on_stopping():
         try:
             stop_malloc()
         except Exception as e:
             LOG.error(e)
-        notifier.notify('STOPPING=1')
-        notifier.notify('STATUS=Stopping')
+        notifier.notify("STOPPING=1")
+        notifier.notify("STATUS=Stopping")
 
     def on_error(err: str):
         if err.isnumeric():
-            notifier.notify(f'ERRNO={err}')
+            notifier.notify(f"ERRNO={err}")
         else:
-            notifier.notify(f'ERRNO=1')
+            notifier.notify("ERRNO=1")
 
     def on_alive():
-        notifier.notify('STATUS=Starting')
+        notifier.notify("STATUS=Starting")
 
     def on_started():
-        notifier.notify('STATUS=Started')
+        notifier.notify("STATUS=Started")
 
-    service(ready_hook=on_ready, error_hook=on_error,
-            stopping_hook=on_stopping, alive_hook=on_alive,
-            started_hook=on_started, **kwargs)
+    service(
+        ready_hook=on_ready,
+        error_hook=on_error,
+        stopping_hook=on_stopping,
+        alive_hook=on_alive,
+        started_hook=on_started,
+        **kwargs,
+    )
 
 
-def start_malloc(config: dict = None, stack_depth: int = 1,
-                 force: bool = False) -> bool:
+def start_health_check_server(service_status: ProcessStatus, port: int = 8000) -> Thread:
+    """
+    Starts an HTTP server to report the status of a module that implements a
+    ProcessStatus object.
+    """
+    import json
+    from http.server import BaseHTTPRequestHandler, HTTPServer
+
+    class HealthCheckHandler(BaseHTTPRequestHandler):
+        def __init__(self, *args, **kwargs):
+            self.service_status: ProcessStatus = kwargs.pop("service_status")
+            BaseHTTPRequestHandler.__init__(self, *args, **kwargs)
+
+            
+        def do_GET(self):
+            if self.path == "/health":
+                if self.service_status.state == ProcessState.NOT_STARTED:
+                    resp_code = 503
+                    content = "Service not started"
+                elif self.service_status.state == ProcessState.STARTED:
+                    resp_code = 200
+                    content = "Starting"
+                elif self.service_status.state == ProcessState.ALIVE:
+                    resp_code = 200
+                    content = "Starting"
+                elif self.service_status.state == ProcessState.READY:
+                    resp_code = 200
+                    content = "Ready"
+                elif self.service_status.state == ProcessState.STOPPING:
+                    resp_code = 503
+                    content = "Stopping"
+                elif self.service_status.state == ProcessState.STOPPED:
+                    resp_code = 503
+                    content = "Stopped"
+                elif self.service_status.state == ProcessState.ERROR:
+                    resp_code = 500
+                    content = "Error"
+                else:
+                    resp_code = 500
+                    content = f"Unknown state: {self.service_status.state}"
+                self.service_status = (
+                    self.server.service_status
+                )  # Assuming you pass this to the server
+                if self.service_status.state == ProcessState.ALIVE:
+                    resp_code = 200
+                    content = "Starting"
+                elif self.service_status.state == ProcessState.READY:
+                    resp_code = 200
+                    content = "Ready"
+                else:
+                    resp_code = 500
+                    content = "Error"
+
+                self.send_response(resp_code)
+                self.send_header("Content-Type", "application/json")
+                self.end_headers()
+                self.wfile.write(
+                    json.dumps({"status": content}).encode("utf-8")
+                )
+            else:
+                self.send_response(404)
+                self.end_headers()
+
+
+    server = HTTPServer(("0.0.0.0", port), HealthCheckHandler, service_status=service_status)
+    thread = Thread(server.serve_forever, daemon=True)
+    thread.start()
+    LOG.info(f"Started health check endpoint at {server.server_address}")
+    return thread
+
+def start_malloc(
+    config: dict = None, stack_depth: int = 1, force: bool = False
+) -> bool:
     """
     Start malloc trace if configured
     :param config: dict Configuration
@@ -90,18 +167,25 @@ def start_malloc(config: dict = None, stack_depth: int = 1,
     """
     if not config:
         from ovos_config.config import Configuration
+
         config = Configuration()
-    if force or config.get('debugging') and \
-            config['debugging'].get('tracemalloc'):
-        LOG.info(f"starting tracemalloc")
+    if (
+        force
+        or config.get("debugging")
+        and config["debugging"].get("tracemalloc")
+    ):
+        LOG.info("starting tracemalloc")
         tracemalloc.start(stack_depth)
-        if config['debugging'].get('log_malloc'):
-            interval_minutes = config['debugging'].get('log_interval_minutes',
-                                                       60)
+        if config["debugging"].get("log_malloc"):
+            interval_minutes = config["debugging"].get(
+                "log_interval_minutes", 60
+            )
             global _malloc_thread
-            _malloc_thread = Thread(target=_log_malloc,
-                                    args=((interval_minutes * 60),),
-                                    daemon=True)
+            _malloc_thread = Thread(
+                target=_log_malloc,
+                args=((interval_minutes * 60),),
+                daemon=True,
+            )
             _malloc_thread.start()
         return True
     return False
@@ -112,7 +196,7 @@ def stop_malloc():
     Stop tracemalloc logging if active
     """
     if _malloc_thread:
-        LOG.debug(f"Stopping malloc logging")
+        LOG.debug("Stopping malloc logging")
         _malloc_event.set()
         _malloc_thread.join()
 
@@ -131,30 +215,37 @@ def snapshot_malloc() -> Optional[tracemalloc.Snapshot]:
     return None
 
 
-def print_malloc(snapshot: tracemalloc.Snapshot, limit: int = 8,
-                 filter_traces: bool = False):
+def print_malloc(
+    snapshot: tracemalloc.Snapshot, limit: int = 8, filter_traces: bool = False
+):
     """
     Log a malloc snapshot
     :param snapshot: Snapshot to evaluate
     :param limit: number of traces to log
     :param filter_traces: if True, remove importlib and unknown stack traces
     """
-    LOG.debug(f"Processing snapshot")
+    LOG.debug("Processing snapshot")
     if filter_traces:
-        snapshot = snapshot.filter_traces((
-            tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
-            tracemalloc.Filter(False, "<frozen importlib._bootstrap_external>"),
-            tracemalloc.Filter(False, "<unknown>"),
-        ))
-    top_stats = snapshot.statistics('traceback')
+        snapshot = snapshot.filter_traces(
+            (
+                tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
+                tracemalloc.Filter(
+                    False, "<frozen importlib._bootstrap_external>"
+                ),
+                tracemalloc.Filter(False, "<unknown>"),
+            )
+        )
+    top_stats = snapshot.statistics("traceback")
 
     LOG.info(f"Top {limit} memory users")
     for index, stat in enumerate(top_stats[:limit], 1):
         frame = stat.traceback[0]
-        LOG.info(f"#{index}: {frame.filename}:{frame.lineno}: "
-                 f"{stat.size / 1048576} MiB")
+        LOG.info(
+            f"#{index}: {frame.filename}:{frame.lineno}: "
+            f"{stat.size / 1048576} MiB"
+        )
         for frame in stat.traceback[1:]:
-            LOG.debug(f'{frame.filename}:{frame.lineno}')
+            LOG.debug(f"{frame.filename}:{frame.lineno}")
 
     other = top_stats[limit:]
     if other:
@@ -168,4 +259,4 @@ def _log_malloc(interval_seconds: int):
     _malloc_event.clear()
     while not _malloc_event.wait(interval_seconds):
         print_malloc(snapshot_malloc(), filter_traces=True)
-    LOG.info(f"Stopping malloc logging")
+    LOG.info("Stopping malloc logging")
